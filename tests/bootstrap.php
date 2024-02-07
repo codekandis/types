7920484883d9b106906b9b0e2662d960fd5faeed
Christian Ramelow <info@codekandis.net>
2024-02-07 15:10:23 +0100

    Adds initial content.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,11 +3,11 @@ namespace CodeKandis\Types\Tests;
 
 use function error_reporting;
 use function ini_set;
+use function sprintf;
 use const E_ALL;
 
 /**
  * Represents the bootstrap script of the tests.
- *
  * @package codekandis/types
  * @author  Christian Ramelow <info@codekandis.net>
  */
@@ -15,4 +15,8 @@ error_reporting( E_ALL );
 ini_set( 'display_errors', 'On' );
 ini_set( 'html_errors', 'Off' );
 
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once sprintf(
+	'%s/%s',
+	dirname( __DIR__, 1 ),
+	'vendor/autoload.php'
+);
