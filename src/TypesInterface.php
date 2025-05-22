18d3442fff84fb426aef7be3549bd68229cf9966
Christian Ramelow <info@codekandis.net>
2025-05-22 22:08:12 +0200

    Makes types instantiable.

--- src/TypesInterface.php
+++ src/TypesInterface.php
@@ -9,23 +9,127 @@ namespace CodeKandis\Types;
 interface TypesInterface
 {
 	/**
-	 * Creates a typed `resource` by a specific resource type.
-	 * @param string $resourceType The type of the `resource`.
-	 * @return string The typed `resource`.
+	 * Gets the type `unknown`.
+	 * @var string
 	 */
-	public static function createTypedResource( string $resourceType ): string;
+	public string $unknownType {
+		get;
+	}
 
 	/**
-	 * Creates a typed `object` by a specific class name.
+	 * Gets the type `null`.
+	 * @var string
+	 */
+	public string $null {
+		get;
+	}
+
+	/**
+	 * Gets the type `resource`.
+	 * @var string
+	 */
+	public string $resource {
+		get;
+	}
+
+	/**
+	 * Gets the template for typed `resource` values.
+	 * @var string
+	 */
+	public string $typedResourceTemplate {
+		get;
+	}
+
+	/**
+	 * Gets the type `closed resource`.
+	 * @var string
+	 */
+	public string $closedResource {
+		get;
+	}
+
+	/**
+	 * Gets the type `array`.
+	 * @var string
+	 */
+	public string $array {
+		get;
+	}
+
+	/**
+	 * Gets the type `object`.
+	 * @var string
+	 */
+	public string $object {
+		get;
+	}
+
+	/**
+	 * Gets the template for typed `object` values.
+	 * @var string
+	 */
+	public string $typedObjectTemplate {
+		get;
+	}
+
+	/**
+	 * Gets the type `bool`.
+	 * @var string
+	 */
+	public string $boolean {
+		get;
+	}
+
+	/**
+	 * Gets the template for typed `bool` values.
+	 * @var string
+	 */
+	public string $typedBooleanTemplate {
+		get;
+	}
+
+	/**
+	 * Gets the type `integer`.
+	 * @var string
+	 */
+	public string $integer {
+		get;
+	}
+
+	/**
+	 * Gets the type `float`.
+	 * @var string
+	 */
+	public string $float {
+		get;
+	}
+
+	/**
+	 * Gets the type `string`.
+	 * @var string
+	 */
+	public string $string {
+		get;
+	}
+
+	/**
+	 * Creates a typed `resource` from a resource type.
+	 * @param string $resourceType The resource type.
+	 * @return string The typed `resource`.
+	 */
+	public function createTypedResource( string $resourceType ): string;
+
+	/**
+	 * Creates a typed `object` from a class name.
 	 * @param string $className The name of the class.
 	 * @return string The typed `object`.
 	 */
-	public static function createTypedObject( string $className ): string;
+	public function createTypedObject( string $className ): string;
 
 	/**
-	 * Creates a typed `boolean` by a specific value.
+	 * Creates a typed `bool` from a boolean value.
 	 * @param bool $value The value.
-	 * @return string The type `boolean`.
+	 * @return string The typed `bool`.
 	 */
-	public static function createTypedBoolean( bool $value ): string;
+	public function createTypedBoolean( bool $value ): string;
 }
